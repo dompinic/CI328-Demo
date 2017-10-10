2c7b2ad720392dfd69988c71f29e2a1fa62afcd4
Dominic Parker <32480334+dompinic@users.noreply.github.com>
2017-10-10 11:55:54 +0100

    Delete 01-basic.js

--- 01-basic.js
+++
@@ -1,9 +0,0 @@
-var game;
-
-function main() {
-    game = new Phaser.Game(800, 600, Phaser.AUTO, '', { create: create });
-}
-
-function create() {
-    game.add.text(50, 50, "Phaser is working!", { fontSize: '32px', fill: '#FFF' });
-}
